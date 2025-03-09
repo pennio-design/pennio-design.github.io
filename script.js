595f1fd1855d3b54abaf3d60e59dc25176d01358
PAUL ADEBISI OYATOWO <106627306+pennio-design@users.noreply.github.com>
2025-03-09 09:24:56 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const instaFeed = document.getElementById("insta-feed");
+    
+    instaFeed.innerHTML = `
+        <iframe src="https://www.instagram.com/pennio__/embed" width="400" height="500" frameborder="0" scrolling="no"></iframe>
+    `;
+});
